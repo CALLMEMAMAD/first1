bf771986b41df7e702d66fb6f394511bb20e1c41
mohammad_harmi <95447590+CALLMEMAMAD@users.noreply.github.com>
2023-04-15 20:55:33 +0330

    Add files via upload

---
+++ lenghandwidthoftherectangle.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    double tool , arz;
+    cout << "enter tool: ";
+    cin  >> tool;
+
+    cout << "enter arz: ";
+    cin >> arz;
+
+    cout <<(tool + arz) *2;
+
+    return 0;
+
+}
